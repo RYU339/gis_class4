e5494a56d684143a3fa4c5e496685bdc88c3a941
RYU <ryuryuryu339@likelion.org>
2021-07-28 17:57:27 +0900

    21.07.28 class 4 commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
@@ -13,6 +11,7 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
+
 
 @login_required(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
@@ -50,8 +49,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
