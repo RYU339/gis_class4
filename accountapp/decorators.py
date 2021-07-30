19cdf45bd44b6f5c0d01bcc8d07a750172e28a73
RYU <ryuryuryu339@likelion.org>
2021-07-30 14:05:29 +0900

    21.07.30 class 4 commit

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseForbidden
 def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_user = User.objects.get(pk=kwargs['pk'])
-        if target_user == request:
+        if target_user == request.user:
             return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
