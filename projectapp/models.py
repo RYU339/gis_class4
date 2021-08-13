dc5f6ae883f4b5aa2dbb21acb789c772ae3fe41c
RYU <ryuryuryu339@likelion.org>
2021-08-13 12:19:48 +0900

    21.08.13 class 4 commit 5

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,1 +1,11 @@
 # Create your models here.
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='project/', null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
