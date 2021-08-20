aeb01ea652818605e33ee8a90183b6390385b3e1
RYU <ryuryuryu339@likelion.org>
2021-08-20 17:41:10 +0900

    21.08.20 class 4 commit 3

--- projectapp/views.py
+++ projectapp/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -28,8 +29,18 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
+        user = self.request.user
+        project = self.object
+
+        if user.is_authenticated: # 로그인 여부 확인
+            subscription = Subscription.objects.filter(user=user,
+                                                       project=project)
+        else:
+            subscription = None
+
         article_list = Article.objects.filter(project=self.object)
         return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
                                         **kwargs)
 
 class ProjectListView(ListView):
