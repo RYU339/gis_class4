2319eb48f822f74e52ea0937800bfa84cbc89b69
RYU <ryuryuryu339@likelion.org>
2021-08-20 18:08:50 +0900

    21.08.20 class 4 commit 4

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -26,3 +27,17 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        # 유저가 구독한 모든 프로젝트를 가져옴 , values_list('project'): 프로젝트만 리스트화해서 가져옴
+        article_list = Article.objects.filter(project__in=project_list)
+        # project_list와 조건이 하나라도 만족하면 가져옴
+        return article_list
