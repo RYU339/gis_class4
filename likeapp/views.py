a9cb6292ac2683191ee000514c58e7b3548827b4
RYU <ryuryuryu339@likelion.org>
2021-08-24 12:45:15 +0900

    21.08.24 class 4 commit 4

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 # Create your views here.
@@ -21,14 +22,19 @@ class LikeArticleView(RedirectView):
         # 이 유저가 이 게시글에 좋아요를 찍었는지 찾아봄
 
         if like_record.exists():
+            # 좋아요 반영 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한 번만 가능합니다')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
         else:
+            # 좋아요 반영 O
             LikeRecord(user=user, article=article).save()
 
         article.like += 1
         article.save()
         # save를 꼭 해줘야 like 추가한 게 반영이 됨
+
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다')
 
         return super().get(request, *args, **kwargs)
 
